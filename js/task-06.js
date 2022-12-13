4973eb4c21c842eb145d5b20aa1aac19d88784ac
VirLg <108211942+VirLg@users.noreply.github.com>
2022-12-13 17:44:22 +0200

    hw

--- js/task-06.js
+++ js/task-06.js
@@ -1,41 +1,28 @@
 // завдання 6
 
 
-const task6 = document.body;
-const inputMarkupFocus = `
-<h2>Task6</h2>
-<input
-  type="text"
-  id="#validation-input"
-  data-length="6"
-  placeholder="Please enter 6 symbols"
-/>
-  <h1 class="add">HYFJKK</h1>
-`
-task6.insertAdjacentHTML('beforeend', inputMarkupFocus)
-
-// const refs = {
-//   inputMarkupRef: document.querySelector('#validation-input')
-  
-// }
-
-const validationLength = refs.inputMarkupRef.dataset.length;
 
 
 
+const task6 = document.body
+const inputRef = document.querySelector("#validation-input")
+
+const validationLength = inputRef.dataset.length
 
 
-// refs.inputMarkupFocusRef.addEventListener('input', onInputFocus)
-const inputValidRefStylyle = document.querySelector('#validation-input.valid') 
-const inputInvalidRefStylyle = ("#validation-input.invalid")
-console.log(inputInvalidRefStylyle);
-refs.inputMarkupRef.addEventListener('blur', onImputBlur)
+
+inputRef.addEventListener('blur', onImputBlur)
 
 function onImputBlur(evn) {
   if ((evn.currentTarget.value).length < `${validationLength}`) {
-    refs.inputMarkupRef.style.borderColor = `${inputInvalidRefStylyle}`
-    console.log(inputInvalidRefStylyle);
+     inputRef.classList.add('invalid')
+    
   } else {
-    refs.inputMarkupRef.style.borderColor = `${inputValidRefStylyle}`
+    inputRef.classList.replace('invalid', "valid")
+
   }
 }
+
+
+
+

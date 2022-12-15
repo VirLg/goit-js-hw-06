e8e83c7fc42700c4967c887caccc6812c541ac83
VirLg <108211942+VirLg@users.noreply.github.com>
2022-12-15 17:19:17 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -13,13 +13,35 @@ const validationLength = inputRef.dataset.length
 
 inputRef.addEventListener('blur', onImputBlur)
 
+
+let a = 6;
+
+
 function onImputBlur(evn) {
   if ((evn.currentTarget.value).length < `${validationLength}`) {
-     inputRef.classList.add('invalid')
     
-  } else {
-    inputRef.classList.replace('invalid', "valid")
+    a = addClass((evn.currentTarget.value).length)
+    
+  } else { 
+   
+    a = addClass((evn.currentTarget.value).length)
+  
+  }
 
+}
+
+function addClass() {
+  
+ 
+  if (!a) {
+      inputRef.classList.remove('invalid')
+    inputRef.classList.add('valid')
+ 
+  } else { 
+   
+       inputRef.classList.add('invalid')
+    inputRef.classList.remove('valid')
+  
   }
 }
 
